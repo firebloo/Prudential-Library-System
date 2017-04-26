269742615899694fecc73d608516c402abea9761
Myeongsu Jeon <jun.tridea@gmail.com>
2017-04-26 04:32:06 +0000

    Prudential Library System, 첫번째 커밋

---
+++ library/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    published_date = models.DateTimeField(blank=True, null=True)
+    category = models.CharField(max_length=200)
+    page = models.IntegerField()
+
+def rental(self):
+    self.save()
+
+def __str__(self):
+    return self.title
