fcf9a3223ad5adf63ac315baf641d046a58aaaa3
Myeongsu Jeon <jun.tridea@gmail.com>
2017-04-28 00:06:39 +0000

    ADded view and template for detailed library book as well as CSS for the site.

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.book_list, name='book_list'),
+    url(r'^book/(?P<pk>[0-9]+)/$', views.book_detail, name='book_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Book
+from django.shortcuts import render, get_object_or_404
 
 def book_list(request):
     books = Book.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'library/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    Book.objects.get(pk=pk)
+    return render(request, 'library/book_detail.html', {'book': book})
+   
