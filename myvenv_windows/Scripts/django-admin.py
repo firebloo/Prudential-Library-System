89886989c92a646f8ae5b1cfeb78c2c9b990c064
Mung <jun.tridea@gmail.com>
2017-05-03 23:01:11 +0900

    Make myvenv_windows for Windows

---
+++ myvenv_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\03_Python\Prudential-Library-System\myvenv_windows\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myvenv_windows/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!D:\03_Python\Prudential-Library-System\myvenv_windows\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==9.0.1','console_scripts','pip'
+__requires__ = 'pip==9.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==9.0.1', 'console_scripts', 'pip')()
+    )
