799121e0defd9eb19b6d9cf9f8b211a6ad2e8bd6
Mung <jun.tridea@gmail.com>
2017-05-05 23:45:36 +0900

    Implement perfect signup page

--- library/forms.py
+++ library/forms.py
@@ -11,17 +11,23 @@ class BookForm(forms.ModelForm):
 
 
 class CreateUserForm(UserCreationForm): # 내장 회원가입 폼을 상속받아서 확장한다.
-    email = forms.EmailField(required=True) # 이메일 필드 추가
-    dept = forms.ComboField('시스템운영팀')
-    #['시스템개발팀', '시스템운영팀', '시스템기획팀', '정보보호팀']
+    SOP = '시스템운영팀'
+    SYD = '시스템개발팀'
+    ISC = '정보보호팀'
+    SYP = '시스템기획팀'
+    dept_choices = ((SOP, "시스템운영팀"),
+                    (SYD, "시스템개발팀"),
+                    (ISC, "정보보호팀"),
+                    (SYP, "시스템기획팀"),
+                    )
+    dept = forms.ChoiceField(choices=dept_choices)
 
     class Meta:
         model = User
-        fields = ("dept", "username", "email", "password1", "password2")
+        fields = ("dept", "username", "password1", "password2")
 
     def save(self, commit=True): # 저장하는 부분 오버라이딩
         user = super(CreateUserForm, self).save(commit=False) # 본인의 부모를 호출해서 저장하겠다.
-        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
