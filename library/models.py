fb8a5d7fef57832d754a47118dc562c19d7cd0c4
Mung <jun.tridea@gmail.com>
2017-05-06 01:32:03 +0900

    대여시스템 구현 중

--- library/models.py
+++ library/models.py
@@ -1,18 +1,36 @@
 from django.db import models
-from django.utils import timezone
+from datetime import date
+#from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Book(models.Model):
-    isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13, primary_key=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publisher = models.CharField(max_length=200)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateField(blank=False, null=False)
     category = models.CharField(max_length=200)
     page = models.IntegerField()
 
+    request_user = models.CharField(max_length=5, null=True)
+    request_date = models.DateField(blank=False, null=False)
+
+    owner_user = models.CharField(max_length=5, null=True)
+
     def rental(self):
-        self.published_date = timezone.now()
+        # self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+
+class RentHistory(models.Model):
+    isbn = models.ForeignKey(Book)
+    rental_date = models.DateField(blank=False, null=False)
+    release_date = models.DateField(blank=False, null=False)
+    rental_user = models.ForeignKey(User)
+
+    def rental(self):
+        self.rental_date = date.today()
+        self.rental_user = User.username
